c3fce3ad27fbfffd7cd83f66cb3a19a0e2b6c773
qazx8855 <qazx8855@hotmail.com>
2022-06-20 23:39:35 +0800

    Delete 2.py

--- 2.py
+++
@@ -1,9 +0,0 @@
-x,y = 5,6
-surrender = 3
-for i in range(0, 2*surrender+1):
-
-    print(x-surrender+i,y-surrender)
-
-print(' ')
-for i in range(1, 2 * surrender):
-    print(x - surrender + i, y - surrender)
